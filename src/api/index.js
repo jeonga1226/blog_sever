7c9dc6870b978fc03d9fe3a63c01720eeee97355
jeonga1226 <shinja1226@gmail.com>
2021-09-28 20:57:02 +0900

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+// const Router = require('koa-router');
+// const posts = require('./posts');\
+import Router from 'koa-router';
+import posts from './posts';
+
+const api = new Router();
+
+api.use('/posts', posts.routes());
+
+// 라우터를 내보냅니다.
+export default api;
